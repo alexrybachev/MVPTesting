a8fc0a818cacd26939ec0b193a4c3c6c5cebaa2f
Alex Rybachev <rybachev.as@gmail.com>
2021-11-02 22:28:06 +0400

    setup ViewController

--- MVPTesting/ViewController.swift
+++ MVPTesting/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properies
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "Please tap to Screen"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 20
+        button.tintColor = .white
+        button.setTitle("Press me", for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupHierarchy()
+        setupLayout()
     }
 
-
+    // MARK: - Setup
+    private func setupHierarchy() {
+        view.backgroundColor = .white
+        view.addSubview(textLabel)
+        view.addSubview(button)
+    }
+    
+    private func setupLayout() {
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        textLabel.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        
+        button.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 160).isActive = true
+        button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
 }
 
