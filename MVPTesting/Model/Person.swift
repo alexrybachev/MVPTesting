f9ceb5f264ac8b841961f4de621785929f8d40cb
Alex Rybachev <rybachev.as@gmail.com>
2021-11-02 22:30:39 +0400

    add Model

---
+++ MVPTesting/Model/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  MVPTesting
+//
+//  Created by Aleksandr Rybachev on 02.11.2021.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
